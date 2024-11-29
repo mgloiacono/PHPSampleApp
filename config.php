0fa6794250eacc1e7ca29451f6b3ae752c89a6c0
Martin Loiacono <mgloiacono@gmail.com>
2024-11-28 22:41:16 -0300

    AWS DB endpoint configured

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = mysqli_connect("localhost", "root", "", "todo")
+$db = mysqli_connect("mysqldbinstance.cb4g68q4ydzq.us-east-2.rds.amazonaws.com", "admin", "admin1234", "todo")
     or
     die("Connection failed: " . mysqli_connect_error());
 
